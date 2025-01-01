decae1a34d936b062964beccdef89831a0bb3fd2
agungfir98 <agungfir98@gmail.com>
2025-01-01 22:26:45 +0700

    feat: dynamic breadcrumb

---
+++ src/components/dynamic-breadcrumb.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { usePathname } from "next/navigation";
+import { Fragment } from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "./ui/breadcrumb";
+
+const DynamicBreadCrumb = () => {
+  const pathName = usePathname();
+  const segments = pathName.split("/").filter(Boolean);
+
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          {segments.length < 1 ? (
+            <BreadcrumbPage>Home</BreadcrumbPage>
+          ) : (
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          )}
+        </BreadcrumbItem>
+
+        {segments.map((segment, index) => {
+          const last = index == segments.length - 1;
+          const href = segments.slice(0, index + 1).join("/");
+
+          return (
+            <Fragment key={index}>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                {last ? (
+                  <BreadcrumbPage>{segment}</BreadcrumbPage>
+                ) : (
+                  <BreadcrumbLink href={href}>{segment}</BreadcrumbLink>
+                )}
+              </BreadcrumbItem>
+            </Fragment>
+          );
+        })}
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+export default DynamicBreadCrumb;
