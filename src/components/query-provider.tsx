09ce94a58fa86c8d0cd1b6b55bedc1f5939b6f03
agungfir98 <agungfir98@gmail.com>
2025-01-02 19:53:12 +0700

    chore: export queryClient

--- src/components/query-provider.tsx
+++ src/components/query-provider.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren } from "react";
 
-const queryClient = new QueryClient();
+export const queryClient = new QueryClient();
 
 export const QueryProvider: React.FC<PropsWithChildren> = ({ children }) => {
   return (
