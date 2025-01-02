e878c14b6cdc60855b640b5584808f391e538cff
agungfir98 <agungfir98@gmail.com>
2025-01-02 10:43:48 +0700

    feat: dashboard layout

---
+++ src/app/(main)/(dashboard)/layout.tsx
@@ -0,0 +1,35 @@
+import { ThemeProvider } from "next-themes";
+import React, { PropsWithChildren } from "react";
+
+import { AppSidebar } from "~/components/app-sidebar";
+import DynamicBreadCrumb from "~/components/dynamic-breadcrumb";
+import { Separator } from "~/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "~/components/ui/sidebar";
+
+const DashboardLayout: React.FC<PropsWithChildren> = ({ children }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12 bg-background">
+            <div className="flex items-center gap-2 px-4">
+              <SidebarTrigger className="-ml-1" />
+              <Separator orientation="vertical" className="mr-2 h-4" />
+              <DynamicBreadCrumb />
+            </div>
+          </header>
+          <div className="flex flex-1 flex-col gap-4 p-4 pt-0 bg-background">
+            {children}
+          </div>
+        </SidebarInset>
+      </SidebarProvider>
+    </ThemeProvider>
+  );
+};
+
+export default DashboardLayout;
