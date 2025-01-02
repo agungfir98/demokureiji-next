940dc95d948e0072cb9e14f189f4a31871e0b1df
agungfir98 <agungfir98@gmail.com>
2025-01-02 16:13:28 +0700

    chore: make state persist

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,14 +1,21 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
 type AuthState = {
   isAuthenticated: boolean;
   token: string | null;
   setToken: (token: string | null) => void;
 };
 
-const useAuthStore = create<AuthState>((set) => ({
-  isAuthenticated: false,
-  token: null,
-  setToken: (token) => set({ isAuthenticated: !!token, token }),
-}));
+const useAuthStore = create<AuthState>()(
+  persist(
+    (set) => ({
+      isAuthenticated: false,
+      token: null,
+      setToken: (token) => set({ isAuthenticated: !!token, token }),
+    }),
+    { name: "authStore", partialize: (state) => ({ token: state.token }) }
+  )
+);
 
 export default useAuthStore;
