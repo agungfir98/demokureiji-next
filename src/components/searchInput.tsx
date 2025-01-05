636ab5e62ee9af7e6e4540e45f87f7f255dd1aa5
agungfir98 <agungfir98@gmail.com>
2025-01-05 15:13:00 +0700

    feat: search input

---
+++ src/components/searchInput.tsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react";
+import React from "react";
+import { cn } from "~/lib/utils";
+
+export const SearchInput = React.forwardRef<
+  HTMLInputElement,
+  React.ComponentProps<"input">
+>(({ className, type, ...props }, ref) => {
+  return (
+    <div className="flex items-center border-b px-3">
+      <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
+      <input
+        type={type}
+        className={cn(
+          "flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    </div>
+  );
+});
