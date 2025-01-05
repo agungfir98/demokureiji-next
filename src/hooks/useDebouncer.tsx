2cdecd3a36a30ea2fe39d4c364abcf37d1c78c30
agungfir98 <agungfir98@gmail.com>
2025-01-05 15:11:25 +0700

    feat: add debouncer

---
+++ src/hooks/useDebouncer.tsx
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+export default function useDebouncer<T>(value: T, interval: number): [T] {
+  const [v, setValue] = useState<T>(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setValue(value);
+    }, interval);
+
+    return () => clearTimeout(handler);
+  }, [value]);
+
+  return [v];
+}
