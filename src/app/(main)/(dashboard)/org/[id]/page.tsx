de576fa94a9292eaff246178bb4a80d52ed261f3
agungfir98 <agungfir98@gmail.com>
2025-01-11 14:10:38 +0700

    feat: add pagination for members

--- src/app/(main)/(dashboard)/org/[id]/page.tsx
+++ src/app/(main)/(dashboard)/org/[id]/page.tsx
@@ -1,27 +1,65 @@
 "use client";
 import { Plus } from "lucide-react";
-import { useParams } from "next/navigation";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
+import { SearchInput } from "~/components/searchInput";
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from "~/components/ui/card";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "~/components/ui/pagination";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "~/components/ui/select";
 import { Separator } from "~/components/ui/separator";
+import { useDebouncedCallback } from "~/hooks/useDebouncer";
 import { orgService } from "~/services/orgService";
-import { IUser } from "~/type/httpResponse";
+import { OrgQueryType } from "~/type/org";
 import { NewMemberModal } from "./components/NewMemberModal";
 import { MemberContextMenu } from "./components/member-context-menu";
 
 const OrgDetail = () => {
   const { id } = useParams();
   const [showNewMemberModal, setShowNewMemberModal] = useState<boolean>(false);
+  const router = useRouter();
+  const [query, setQuery] = useState<OrgQueryType>({
+    memberLimit: 10,
+    memberPage: 1,
+  });
 
-  const { data } = orgService.GetSingleOrg({ orgId: id as string });
+  const { data, isPending } = orgService.GetSingleOrg(
+    { orgId: id as string },
+    { ...query },
+    {
+      queryKey: [query, "orgDetail", id],
+    }
+  );
+
+  const handleSearchMember = useDebouncedCallback((searchMember: string) => {
+    if (searchMember) {
+      return setQuery({ memberPage: 1, memberLimit: 1, searchMember });
+    } else {
+      return setQuery({ memberPage: 1, memberLimit: 10 });
+    }
+  }, 750);
 
   return (
     <div className="grid gap-4">
@@ -34,9 +72,7 @@ const OrgDetail = () => {
                   {data?.data.data?.organization}
                 </h1>
               </CardTitle>
-              <CardDescription>
-                {data?.data.data?.description} kebiasaan, anjing
-              </CardDescription>
+              <CardDescription>{data?.data.data.description}</CardDescription>
             </div>
           </div>
         </CardHeader>
@@ -44,26 +80,45 @@ const OrgDetail = () => {
           <div>
             <div className="flex justify-between my-2">
               <h1 className="text-xl font-semibold">Events</h1>
-              {data?.data.data!.isAdmin && (
-                <div className="">
-                  <Button size="sm">
-                    <Plus />
-                    New Event
-                  </Button>
-                </div>
-              )}
+              {data?.data.data!.role === "admin" ||
+                (data?.data.data?.role === "master" && (
+                  <div className="">
+                    <Button
+                      size="sm"
+                      onClick={() => {
+                        return router.replace(`/org/${id}/create-event`);
+                      }}
+                    >
+                      <Plus />
+                      New Event
+                    </Button>
+                  </div>
+                ))}
             </div>
             <Card>
               <CardContent className="mt-6 grid">
-                {data?.data.data?.voteEvents.length === 0 && (
+                {!data?.data.data?.voteevents.length && (
                   <span className="my-4 mx-auto opacity-65">
                     no event being held
                   </span>
                 )}
                 <ul>
-                  {data?.data.data?.voteEvents.map((v, index) => (
+                  {data?.data.data?.voteevents.map((event, index) => (
                     <li key={index}>
-                      <p>{v.voteTitle}</p>
+                      <Link
+                        href={`/org/${id}/event/${event._id}`}
+                        className="relative"
+                      >
+                        <Card>
+                          <CardHeader>
+                            <CardTitle>{event.voteTitle}</CardTitle>
+                            <CardDescription className="flex gap-2 items-center">
+                              <div className="w-2 h-2 rounded-full bg-green-500"></div>
+                              <p>{event.status}</p>
+                            </CardDescription>
+                          </CardHeader>
+                        </Card>
+                      </Link>
                     </li>
                   ))}
                 </ul>
@@ -71,45 +126,151 @@ const OrgDetail = () => {
             </Card>
           </div>
           <Separator className="my-4" />
+
           <div>
             <div className="flex justify-between my-2">
               <h1 className="text-xl font-semibold">Members</h1>
-              {data?.data.data!.isAdmin && (
-                <div className="">
-                  <Button size="sm" onClick={() => setShowNewMemberModal(true)}>
-                    <Plus />
-                    add member
-                  </Button>
-                </div>
-              )}
+              {data?.data.data.role === "admin" ||
+                (data?.data.data?.role === "master" && (
+                  <div className="">
+                    <Button
+                      size="sm"
+                      onClick={() => setShowNewMemberModal(true)}
+                    >
+                      <Plus />
+                      add member
+                    </Button>
+                  </div>
+                ))}
             </div>
+
             <Card>
-              <CardContent className="mt-6 grid">
+              <CardContent>
+                <CardHeader>
+                  <SearchInput
+                    placeholder="search member"
+                    onChange={(e) => handleSearchMember(e.target.value)}
+                  />
+                </CardHeader>
                 <ul>
                   {data?.data.data?.members.map((member, index) => {
-                    const v = member as IUser & { isAdmin: boolean };
                     return (
                       <li key={index}>
-                        {data.data.data?.isAdmin ? (
-                          <MemberContextMenu member={v}>
-                            <p>{v.name}</p>
-                            {v.isAdmin && (
-                              <Badge variant="outline">Admin</Badge>
-                            )}
-                          </MemberContextMenu>
-                        ) : (
-                          <div className="flex justify-between hover:bg-secondary p-2">
-                            <p>{v.name}</p>
-                            {v.isAdmin && (
-                              <Badge variant="outline">Admin</Badge>
+                        {data.data.data?.role === "member" ? (
+                          <div className="flex items-center justify-between hover:bg-secondary p-2">
+                            <span className="flex gap-2 items-end">
+                              <p>{member.member.name}</p>
+                              <p className="text-sm font-thin">
+                                {member.member.email}
+                              </p>
+                            </span>
+                            {member.role !== "member" && (
+                              <Badge variant="outline">{member.role}</Badge>
                             )}
                           </div>
+                        ) : (
+                          <MemberContextMenu member={member}>
+                            <div className="flex justify-between items-center hover:bg-secondary p-2">
+                              <span className="flex gap-2 items-center">
+                                <p>{member.member.name}</p>
+                                <p className="text-sm font-thin">
+                                  {member.member.email}
+                                </p>
+                              </span>
+                              {member.role !== "member" && (
+                                <Badge variant="outline">{member.role}</Badge>
+                              )}
+                            </div>
+                          </MemberContextMenu>
                         )}
                         <Separator />
                       </li>
                     );
                   })}
                 </ul>
+
+                <CardFooter>
+                  {!isPending && data && (
+                    <Pagination className="mt-4 w-fit gap-4">
+                      <PaginationContent defaultValue={1}>
+                        <PaginationItem>
+                          <PaginationPrevious
+                            isActive={query.memberPage === 1}
+                            onClick={() => {
+                              if (query.memberPage > 1) {
+                                return setQuery((prev) => ({
+                                  ...prev,
+                                  memberPage: prev.memberPage - 1,
+                                }));
+                              }
+                            }}
+                          />
+                        </PaginationItem>
+                        {Array(data.data.data.pagination.totalPages)
+                          .fill(0)
+                          .map((_, index) => (
+                            <PaginationItem key={index}>
+                              <PaginationLink
+                                isActive={
+                                  data.data.data.pagination.page === index + 1
+                                }
+                                onClick={() => {
+                                  setQuery((prev) => ({
+                                    ...prev,
+                                    memberPage: index + 1,
+                                  }));
+                                }}
+                              >
+                                {index + 1}
+                              </PaginationLink>
+                            </PaginationItem>
+                          ))}
+                        <PaginationItem>
+                          <PaginationNext
+                            isActive={
+                              query.memberPage ===
+                              data.data.data.pagination.totalPages
+                            }
+                            onClick={() => {
+                              if (
+                                query.memberPage <
+                                data.data.data.pagination.totalPages
+                              ) {
+                                return setQuery((prev) => ({
+                                  ...prev,
+                                  memberPage: prev.memberPage + 1,
+                                }));
+                              }
+                            }}
+                          />
+                        </PaginationItem>
+                      </PaginationContent>
+
+                      <Select
+                        defaultValue={data.data.data.pagination.limit.toString()}
+                        onValueChange={(memberLimit) => {
+                          setQuery((prev) => ({
+                            memberPage: Math.ceil(
+                              ((prev.memberPage - 1) * prev.memberLimit + 1) /
+                                Number(memberLimit)
+                            ),
+                            memberLimit: Number(memberLimit),
+                          }));
+                        }}
+                      >
+                        <SelectTrigger>
+                          <SelectValue>{query.memberLimit}</SelectValue>
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="5">5</SelectItem>
+                          <SelectItem value="10">10</SelectItem>
+                          <SelectItem value="15">15</SelectItem>
+                          <SelectItem value="20">20</SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </Pagination>
+                  )}
+                </CardFooter>
               </CardContent>
             </Card>
           </div>
