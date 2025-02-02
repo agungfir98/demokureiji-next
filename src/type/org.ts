7a6d84341d8aa6dc8cd424eb9e8ae70252ad776a
agungfir98 <agungfir98@gmail.com>
2025-02-02 14:02:47 +0700

    fix: org detail query schema

--- src/type/org.ts
+++ src/type/org.ts
@@ -6,3 +6,17 @@ export const newOrgSchema = z.object({
 });
 
 export type NewOrgType = z.infer<typeof newOrgSchema>;
+
+export const orgQuerySchema = z.object({
+  memberLimit: z
+    .string()
+    .default("5")
+    .transform((v) => Number(v)),
+  memberPage: z
+    .string()
+    .default("1")
+    .transform((v) => Number(v)),
+  searchMember: z.string().optional(),
+});
+
+export type OrgQueryType = z.infer<typeof orgQuerySchema>;
