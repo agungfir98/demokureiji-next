6ca665ef2f8fb645229246ac84b4d8dab61cc492
agungfir98 <agungfir98@gmail.com>
2025-04-01 14:29:41 +0700

    chore: remove unused property

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -78,7 +78,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarContent>
         <ScrollArea>
           <NavMain items={data.navMain} />
-          <NavOrg organization={data.organizations} />
+          <NavOrg />
         </ScrollArea>
       </SidebarContent>
       <SidebarFooter>
