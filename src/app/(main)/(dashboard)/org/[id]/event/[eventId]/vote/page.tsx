fb0c7f3cd45b6a4e5d71ddd65c3b37aa456c5751
Agung Firmansyah <agungfir98@gmail.com>
2025-06-10 12:30:23 +0700

    style: prettier

--- src/app/(main)/(dashboard)/org/[id]/event/[eventId]/vote/page.tsx
+++ src/app/(main)/(dashboard)/org/[id]/event/[eventId]/vote/page.tsx
@@ -1,18 +1,29 @@
-"use client"
+"use client";
 import { CheckIcon, LoaderCircle } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 import { Button } from "~/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "~/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { cn } from "~/lib/utils";
 import eventService from "~/services/event.service";
 
 const VotePage = () => {
-  const { eventId, id: orgId } = useParams<{ eventId: string, id: string }>()
-  const [selected, setSelected] = useState<{ candidateId: string | null }>({ candidateId: null })
+  const { eventId, id: orgId } = useParams<{ eventId: string; id: string }>();
+  const [selected, setSelected] = useState<{ candidateId: string | null }>({
+    candidateId: null,
+  });
 
-  const { data } = eventService.GetEvent(eventId as string, { orgId: orgId as string })
+  const { data } = eventService.GetEvent(eventId as string, {
+    orgId: orgId as string,
+  });
 
   const { mutate, isPending } = eventService.Vote({
     onError: err => {
@@ -23,22 +34,33 @@ const VotePage = () => {
   return (
     <main>
       <CardHeader>
-        <CardTitle className="text-2xl">
-          {data?.voteTitle}
-        </CardTitle>
+        <CardTitle className="text-2xl">{data?.voteTitle}</CardTitle>
         <CardDescription>
           <h1>Please select candidate you want to vote</h1>
         </CardDescription>
       </CardHeader>
       <CardContent>
         <div className="grid md:grid-cols-2 gap-4">
-          {data?.candidates.map(v => (
-            <Card key={v._id}
-              className={cn("cursor-pointer select-none relative", v._id == selected.candidateId && "shadow-white/20 shadow-md")}
-              onClick={() => setSelected((prev) => ({ candidateId: prev.candidateId == v._id ? null : v._id }))}>
+          {data?.candidates.map((v) => (
+            <Card
+              key={v._id}
+              className={cn(
+                "cursor-pointer select-none relative",
+                v._id == selected.candidateId && "shadow-white/20 shadow-md",
+              )}
+              onClick={() =>
+                setSelected((prev) => ({
+                  candidateId: prev.candidateId == v._id ? null : v._id,
+                }))
+              }
+            >
               <CardHeader className="flex flex-col gap-2">
-                <CardTitle>{v.calonKetua} {v.calonWakil}</CardTitle>
-                {selected.candidateId == v._id && <CheckIcon className="absolute right-5 top-0" />}
+                <CardTitle>
+                  {v.calonKetua} {v.calonWakil}
+                </CardTitle>
+                {selected.candidateId == v._id && (
+                  <CheckIcon className="absolute right-5 top-0" />
+                )}
               </CardHeader>
               <CardContent>
                 <CardDescription>description</CardDescription>
@@ -52,16 +74,15 @@ const VotePage = () => {
         <Button
           disabled={!selected.candidateId}
           onClick={() => {
-            mutate({ orgId, eventId, candidateId: selected.candidateId! })
-          }}>
+            mutate({ orgId, eventId, candidateId: selected.candidateId! });
+          }}
+        >
           vote
-          <LoaderCircle
-            className={`animate-spin ${!isPending && "hidden"}`}
-          />
+          <LoaderCircle className={`animate-spin ${!isPending && "hidden"}`} />
         </Button>
       </CardFooter>
     </main>
-  )
-}
+  );
+};
 
-export default VotePage
+export default VotePage;
