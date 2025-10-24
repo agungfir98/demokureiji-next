94abe02cffb9bdc5c4f0a77d546067c1d68806fd
agungfir98 <agungfir98@gmail.com>
2025-10-24 21:31:37 +0700

    feat: hasRole utils function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,14 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { OrgMemberRole } from "~/type/org.type";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function hasRole(
+  role: OrgMemberRole,
+  allowedRole: OrgMemberRole[],
+): boolean {
+  return allowedRole.includes(role);
 }
