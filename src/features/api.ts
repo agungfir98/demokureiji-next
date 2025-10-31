8c2455885277e5c339eacc4561f4264b7c85c4b4
agungfir98 <agungfir98@gmail.com>
2025-10-31 19:00:25 +0700

    feat: refresh token with axios interceptor

--- src/features/api.ts
+++ src/features/api.ts
@@ -1,9 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import useAuthStore from "~/hooks/useAuth";
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export type HttpResponse<TData = any> = {
-  success: boolean;
+  ok: boolean;
   message: string;
   data: TData;
   timestamp: string;
@@ -15,8 +15,84 @@ export const axiosInstance = axios.create({
   headers: {},
 });
 
-axiosInstance.interceptors.request.use((config) => {
-  const { token } = useAuthStore.getState();
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const { token } = useAuthStore.getState();
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  },
+  (err) => Promise.reject(err),
+);
+
+let isRefreshing = false;
+let failedQueue: Array<{
+  resolve: (token: string) => void;
+  reject: (error: Error) => void;
+}> = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) {
+      prom.reject(error);
+    } else {
+      prom.resolve(token!);
+    }
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status !== 401 || originalRequest._retry) {
+      return Promise.reject(error);
+    }
+
+    if (isRefreshing) {
+      return new Promise((resolve, reject) => {
+        failedQueue.push({ resolve, reject });
+      })
+        .then((token) => {
+          originalRequest.headers["Authorization"] = `Bearer ${token}`;
+          return axiosInstance(originalRequest);
+        })
+        .catch((err) => Promise.reject(err));
+    }
+
+    originalRequest._retry = true;
+    isRefreshing = true;
+
+    try {
+      const { data } = await axios.post<HttpResponse<{ accessToken: string }>>(
+        "/api/auth/refresh-token",
+        {},
+        {
+          withCredentials: true,
+        },
+      );
+
+      const newAccessToken = data.data.accessToken;
+
+      useAuthStore.getState().setToken(newAccessToken);
+
+      axiosInstance.defaults.headers.common["Authorization"] =
+        `Bearer ${newAccessToken}`;
+      originalRequest.headers["Authorization"] = `Bearer ${newAccessToken}`;
+
+      processQueue(null, newAccessToken);
+
+      return axiosInstance(originalRequest);
+    } catch (refreshError) {
+      processQueue(refreshError, null);
+
+      useAuthStore.getState().setToken(null);
+      window.location.href = "/login";
+
+      return Promise.reject(refreshError);
+    } finally {
+      isRefreshing = false;
+    }
+  },
+);
