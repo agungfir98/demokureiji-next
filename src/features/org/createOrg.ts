fb65154bed10981b6599f22830d079ed453be70d
agungfir98 <agungfir98@gmail.com>
2025-11-04 14:19:37 +0700

    feat: add idempotency

--- src/features/org/createOrg.ts
+++ src/features/org/createOrg.ts
@@ -3,29 +3,36 @@ import { axiosInstance, HttpResponse } from "../api";
 import { z } from "zod";
 import { MutationConfig, queryClient } from "~/lib/query-client";
 import { getOrgQueryKey } from ".";
+import { setIdempotenHeader } from "~/lib/utils";
 
 export const createOrgSchema = z.object({
+  id: z.string().uuid(),
   name: z.string().min(3),
   description: z.string().optional(),
 });
 
 export type createOrgRequest = z.infer<typeof createOrgSchema>;
 
-export const createOrganization = async (data: createOrgRequest) => {
-  const response = await axiosInstance.post<HttpResponse>("/org/new", data);
+export const createOrganization = async (payload: createOrgRequest) => {
+  const response = await axiosInstance.post<HttpResponse>("/org", payload, {
+    headers: {
+      ...setIdempotenHeader(payload.id)
+    }
+  });
   return response.data;
 };
-
 type UseCreateOrgConfig = MutationConfig<typeof createOrganization>;
+
 export const useCreateOrg = (mutationConfig: UseCreateOrgConfig = {}) => {
+  const { onSuccess, ...restConfig } = mutationConfig || {}
   return useMutation({
-    ...mutationConfig,
-    mutationFn: createOrganization,
-    onSuccess: (data, variables, ctx) => {
+    onSuccess: (...args) => {
       queryClient.invalidateQueries({
         queryKey: getOrgQueryKey(),
       });
-      mutationConfig?.onSuccess?.(data, variables, ctx);
+      onSuccess?.(...args);
     },
+    ...restConfig,
+    mutationFn: createOrganization,
   });
 };
